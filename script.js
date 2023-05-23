d2b76835c16c877fcc0de5cc9e5ab6e95f8470d5
imtritesh <103854328+imtritesh@users.noreply.github.com>
2023-05-23 17:18:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function highlight() {
     //Write your code here
-
+	let strongs = document.querySelectorAll("strong");
+	// [strong1, strong2 ,,,,,]
+	strongs.forEach((strong)=>{
+		strong.style.color = "green";
+	})
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+	let strongs = document.querySelectorAll("strong");// strong1
+	strongs.forEach((strong)=>{
+		strong.style.color = "black";
+	})
     
 }
